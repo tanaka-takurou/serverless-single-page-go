e80a34f70fc6c176f3b0835373259767bd824f89
tanaka.takurou <tanaka.takurou@icloud.com>
2020-04-12 12:13:20 +0900

    fix README, scripts

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
-# serverless-single-page-go
+# serverless-single-page kit
+Simple kit for serverless single web page using AWS Lambda.
 
-### Dependence
+
+## Dependence
 - aws-lambda-go
+
+
+## Requirements
+- AWS (Lambda, API Gateway)
 - aws-cli
+- golang environment
+
+
+## Usage
+
+### Edit View
+##### HTML
+- Edit templates/index.html
+
+##### CSS
+- Edit static/css/main.css
+
+##### Javascript
+- Edit static/js/main.js
+
+##### Image
+- Add image file into static/img/
+- Edit templates/index.html like as 'sample_jpg'.
+- Edit create_template.sh like as 'sample_jpg'. (add three lines)
 
 ### Deploy
 Open scripts/deploy.sh and edit 'your_function_name' first.
+Then run this command.
 
 ```
 $ sh scripts/deploy.sh
--- scripts/create_template.sh
+++ scripts/create_template.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-echo -n '{{define "main_css"}}' > `dirname $0`/../templates/view.html
-cat `dirname $0`/../static/css/main.css >> `dirname $0`/../templates/view.html
-echo '{{end}}' >> `dirname $0`/../templates/view.html
+BASE_DIR=`dirname $0`/../
+TARGET_FILE=${BASE_DIR}templates/view.html
 
-echo -n '{{define "main_js"}}' >> `dirname $0`/../templates/view.html
-cat `dirname $0`/../static/js/main.js >> `dirname $0`/../templates/view.html
-echo '{{end}}' >> `dirname $0`/../templates/view.html
+echo -n '{{define "main_css"}}' > ${TARGET_FILE}
+cat ${BASE_DIR}static/css/main.css >> ${TARGET_FILE}
+echo '{{end}}' >> ${TARGET_FILE}
 
-echo -n '{{define "sample_jpg"}}data:image/jpeg;base64,' >> `dirname $0`/../templates/view.html
-base64 -i `dirname $0`/../static/img/sample.jpg >> `dirname $0`/../templates/view.html
-echo '{{end}}' >> `dirname $0`/../templates/view.html
+echo -n '{{define "main_js"}}' >> ${TARGET_FILE}
+cat ${BASE_DIR}static/js/main.js >> ${TARGET_FILE}
+echo '{{end}}' >> ${TARGET_FILE}
 
-echo -n '{{define "favicon"}}data:image/x-icon;base64,' >> `dirname $0`/../templates/view.html
-base64 -i `dirname $0`/../static/img/sample_favicon.ico >> `dirname $0`/../templates/view.html
-echo '{{end}}' >> `dirname $0`/../templates/view.html
+echo -n '{{define "sample_jpg"}}data:image/jpeg;base64,' >> ${TARGET_FILE}
+base64 -i ${BASE_DIR}static/img/sample.jpg >> ${TARGET_FILE}
+echo '{{end}}' >> ${TARGET_FILE}
+
+echo -n '{{define "favicon"}}data:image/x-icon;base64,' >> ${TARGET_FILE}
+base64 -i ${BASE_DIR}static/img/sample_favicon.ico >> ${TARGET_FILE}
+echo '{{end}}' >> ${TARGET_FILE}
