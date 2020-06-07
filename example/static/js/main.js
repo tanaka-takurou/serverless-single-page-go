3070ae039303496d71aaaa432a05a81537d9ec07
tanaka.takurou <tanaka.takurou@icloud.com>
2020-06-07 17:09:39 +0900

    fix example. scroll top

--- example/static/js/main.js
+++ example/static/js/main.js
@@ -1,31 +1,6 @@
 $(document).ready(function() {
-
-  var
-    $headers     = $('body > h1'),
-    $header      = $headers.first(),
-    ignoreScroll = false,
-    timer;
-
-  $(window)
-    .on('resize', function() {
-      clearTimeout(timer);
-      $headers.visibility('disable callbacks');
-
-      $(document).scrollTop( $header.offset().top );
-
-      timer = setTimeout(function() {
-        $headers.visibility('enable callbacks');
-      }, 500);
-    });
-  $headers
-    .visibility({
-      once: false,
-      checkOnRefresh: true,
-      onTopPassed: function() {
-        $header = $(this);
-      },
-      onTopPassedReverse: function() {
-        $header = $(this);
-      }
+  $("#floatingButton")
+    .click(function() {
+      $(document).scrollTop(0);
     });
 });
